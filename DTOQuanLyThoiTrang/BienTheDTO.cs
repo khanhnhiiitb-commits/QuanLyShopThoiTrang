a767c2c016f102af1656cbd58eeeec5cf75b3d5a
Ngô Thị Thu Duyên <duynngo2612@gmail.com>
2026-05-20 22:14:09 +0700

    BientheDTO + SanPhamDTO

--- DTOQuanLyThoiTrang/BienTheDTO.cs
+++ DTOQuanLyThoiTrang/BienTheDTO.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace DTOQuanLyThoiTrang
 {
-    internal class BienTheDTO
+    public class BienTheDTO
     {
+        private string _maBienThe;
+        private string _maSP;
+        private string _mauSac;
+        private string _kichCo;
+        private int _dinhMucToiThieu;
+        private int _soLuongTon;
+        private string _moTa;
+
+        public string MaBienThe { get => _maBienThe; set => _maBienThe = value; }
+        public string MaSP { get => _maSP; set => _maSP = value; }
+        public string MauSac { get => _mauSac; set => _mauSac = value; }
+        public string KichCo { get => _kichCo; set => _kichCo = value; }
+        public int DinhMucToiThieu { get => _dinhMucToiThieu; set => _dinhMucToiThieu = value; }
+        public int SoLuongTon { get => _soLuongTon; set => _soLuongTon = value; }
+        public string MoTa { get => _moTa; set => _moTa = value; }
+
+        public BienTheDTO(string MaBienThe, string MaSP, string MauSac, string KichCo, int DinhMucToiThieu, int SoLuongTon, string MoTa)
+        {
+            _maBienThe = MaBienThe;
+            _maSP = MaSP;
+            _mauSac = MauSac;
+            _kichCo = KichCo;
+            _moTa = MoTa;
+            _dinhMucToiThieu = DinhMucToiThieu;
+            _soLuongTon = SoLuongTon;
+        }
+
+        public BienTheDTO()
+        {
+        }
     }
 }
--- DTOQuanLyThoiTrang/SanPhamDTO.cs
+++ DTOQuanLyThoiTrang/SanPhamDTO.cs
@@ -6,9 +6,33 @@ using System.Threading.Tasks;
 
 namespace DTOShopThoiTrang
 {
-    internal class SanPhamDTO
+    public class SanPhamDTO
     {
-        static void Main(string[] args)
+        private string _maSP;
+        private string _maLoai;
+        private string _tenSP;
+        private decimal _giaNhap;
+        private decimal _giaBan;
+        private string _moTa;
+
+        public string MaSP { get => _maSP; set => _maSP = value; }
+        public string MaLoai { get => _maLoai; set => _maLoai = value; }
+        public string TenSP { get => _tenSP; set => _tenSP = value; }
+        public decimal GiaNhap { get => _giaNhap; set => _giaNhap = value; }
+        public decimal GiaBan { get => _giaBan; set => _giaBan = value; }
+        public string MoTa { get => _moTa; set => _moTa = value; }
+
+        public SanPhamDTO(string maSP, string maLoai, string tenSP, decimal giaBan, decimal giaNhap, string moTa)
+        {
+            _maSP = maSP;
+            _maLoai = maLoai;
+            _tenSP = tenSP;
+            _giaNhap = giaNhap;
+            _giaBan = giaBan;
+            _moTa = moTa;
+        }
+
+        public SanPhamDTO()
         {
         }
     }
