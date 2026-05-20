ced306175cdd426733ebbbb34881ac151ee8a512
Tang Khanh Nhi <KhanhNhiiitb@gmail.com>
2026-05-20 14:29:27 +0700

    update NhanVienDTO và TaiKhoanRepository

--- DALShopThoiTrang/DBConnection.cs
+++ DALShopThoiTrang/DBConnection.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DALShopThoiTrang
 {
-    internal class DBConnection
+    public class DBConnection
     {
+        // Thay connectionString bằng chuỗi kết nối thực tế của bạn
+        private string connectionString = @"Data Source=DESKTOP-HD4B2VM;Initial Catalog=QuanLyShopThoiTrang;Integrated Security=True;TrustServer Certificate=True;"; //KhanhNhi
+        //private string connectionString = @"Server =.\SQLEXPRESS;Database=QuanLyShopThoiTrang;Integrated Security = True;";
+        protected SqlConnection conn;
+
+        public DBConnection()
+        {
+            conn = new SqlConnection(connectionString);
+        }
+
+        public void OpenConnection()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
     }
 }
--- DTOQuanLyThoiTrang/NhanVienDTO.cs
+++ DTOQuanLyThoiTrang/NhanVienDTO.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DTOQuanLyThoiTrang
+namespace DTOShopThoiTrang
 {
-    internal class NhanVienDTO
+    public class NhanVienDTO
     {
+        public string MaNV { get; set; }
+        public string TenNV { get; set; }
+        public string SoDienThoai { get; set; }
+        public string ChucVu { get; set; }
+        public string TrangThai { get; set; }
+        public string MatKhau { get; set; }
+        public string PhanQuyen { get; set; }
+        public string GioiTinh { get; set; }
     }
 }
