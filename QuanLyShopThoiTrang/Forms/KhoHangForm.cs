517cb59809e944d31b8e1b952d1a8644737dcd4a
Tang Khanh Nhi <KhanhNhiiitb@gmail.com>
2026-05-20 13:58:48 +0700

    Cập nhật Cấu trúc thư mục các lớp

--- QuanLyShopThoiTrang/Forms/KhoHangForm.cs
+++ QuanLyShopThoiTrang/Forms/KhoHangForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QuanLyShopThoiTrang
 {
-    public partial class Form1 : Form
+    public partial class KhoHangForm : Form
     {
-        public Form1()
+        public KhoHangForm()
         {
             InitializeComponent();
         }
