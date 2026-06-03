fbca116f0c2265db3c37e1504323fc06adca8372
Ngô Thị Thu Duyên <duynngo2612@gmail.com>
2026-06-03 23:54:56 +0700

    ucDashboard

--- QuanLyShopThoiTrang/UserControls/ucDashboard.Designer.cs
+++ QuanLyShopThoiTrang/UserControls/ucDashboard.Designer.cs
@@ -28,18 +28,285 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dgvStaff = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStaff)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(11, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chào buổi sáng,admin";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.chart1);
+            this.panel1.Location = new System.Drawing.Point(18, 96);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(576, 375);
+            this.panel1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(19, 82);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Color = System.Drawing.Color.Maroon;
+            series3.Legend = "Legend1";
+            series3.Name = "Doanh thu";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(537, 270);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(34, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(35, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tổng doanh thu";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.chart2);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(618, 96);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(555, 341);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.MistyRose;
+            this.panel3.Controls.Add(this.label9);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Location = new System.Drawing.Point(18, 503);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(574, 149);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Controls.Add(this.dgvStaff);
+            this.panel4.Controls.Add(this.label7);
+            this.panel4.Location = new System.Drawing.Point(619, 456);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(554, 196);
+            this.panel4.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(3, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(208, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Top sản phẩm bán chạy";
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            this.chart2.Location = new System.Drawing.Point(31, 89);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series4.Color = System.Drawing.Color.Maroon;
+            series4.IsValueShownAsLabel = true;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(488, 209);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(57, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Cảnh báo tồn kho";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.ErrorImage = global::QuanLyShopThoiTrang.Properties.Resources.cancel_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24;
+            this.pictureBox1.Image = global::QuanLyShopThoiTrang.Properties.Resources.cancel_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24;
+            this.pictureBox1.InitialImage = global::QuanLyShopThoiTrang.Properties.Resources.cancel_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 25);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Maroon;
+            this.label6.Location = new System.Drawing.Point(251, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(13, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 20);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Staff";
+            // 
+            // dgvStaff
+            // 
+            this.dgvStaff.BackgroundColor = System.Drawing.Color.White;
+            this.dgvStaff.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStaff.Location = new System.Drawing.Point(22, 46);
+            this.dgvStaff.Name = "dgvStaff";
+            this.dgvStaff.RowHeadersWidth = 51;
+            this.dgvStaff.RowTemplate.Height = 24;
+            this.dgvStaff.Size = new System.Drawing.Size(516, 112);
+            this.dgvStaff.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(58, 62);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(177, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Số sản phẩm tồn kho hiện tại";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(58, 95);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(196, 16);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Xem chi tiết hơn ở mục kho hàng";
             // 
             // ucDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
             this.Name = "ucDashboard";
             this.Size = new System.Drawing.Size(1192, 729);
+            this.Load += new System.EventHandler(this.ucDashboard_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStaff)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dgvStaff;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
     }
 }
--- QuanLyShopThoiTrang/UserControls/ucDashboard.cs
+++ QuanLyShopThoiTrang/UserControls/ucDashboard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BUSShopThoiTrang;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,145 @@ namespace QuanLyShopThoiTrang.UserControls
 {
     public partial class ucDashboard : UserControl
     {
+        private ThongKeBUS _thongKeBus = new ThongKeBUS();
+        private TaiKhoanBUS _taiKhoanBus = new TaiKhoanBUS();
         public ucDashboard()
         {
             InitializeComponent();
         }
+        // SỰ KIỆN KHI MỞ GIAO DIỆN 
+        private void ucDashboard_Load(object sender, EventArgs e)
+        {
+            LoadTongDoanhThu();
+            LoadCanhBaoTonKho();
+            LoadChartDoanhThu();
+            LoadChartTopSanPham();
+            LoadDanhSachNhanVien();
+        }
+        // 1. TÍNH TOÁN & HIỂN THỊ TỔNG DOANH THU LÊN LABEL2
+        private void LoadTongDoanhThu()
+        {
+            try
+            {
+                // Gọi hàm lấy doanh thu theo ngày của bạn
+                var dtDoanhThu = _thongKeBus.LayDoanhThuTheoNgay();
+                decimal tongDoanhThu = 0;
+
+                // Chạy vòng lặp cộng dồn cột DoanhThu của tất cả các ngày
+                foreach (DataRow row in dtDoanhThu.Rows)
+                {
+                    if (row["DoanhThu"] != DBNull.Value)
+                    {
+                        tongDoanhThu += Convert.ToDecimal(row["DoanhThu"]);
+                    }
+                }
+
+                // Gán vào label2 kèm format có dấu phẩy (VD: 15,000,000 VNĐ)
+                label2.Text = tongDoanhThu.ToString("N0") + " VNĐ";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tải tổng doanh thu: " + ex.Message);
+            }
+        }
+
+        // 2. ĐẾM SỐ LƯỢNG MẶT HÀNG TỒN KHO LÊN LABEL6
+        private void LoadCanhBaoTonKho()
+        {
+            try
+            {
+                // Dùng hàm có sẵn của bạn để lấy mặt hàng dưới định mức
+                var dtTonKho = _thongKeBus.LayHangTonKhoDuoiDinhMuc();
+
+                // Đếm số dòng (tương ứng với số lượng mặt hàng sắp hết)
+                int soLuongCanhBao = dtTonKho.Rows.Count;
+
+                // Gán vào label6
+                label6.Text = soLuongCanhBao.ToString() + " mặt hàng";
+
+                // (Tùy chọn): Đổ luôn danh sách này vào DataGridView lưới bên dưới
+                // dgvStaff.DataSource = dtTonKho; 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tải cảnh báo tồn kho: " + ex.Message);
+            }
+        }
+
+        // 3. ĐỔ DỮ LIỆU VÀO BIỂU ĐỒ CỘT (DOANH THU)
+        private void LoadChartDoanhThu()
+        {
+            try
+            {
+                var dtDoanhThu = _thongKeBus.LayDoanhThuTheoNgay();
+
+                // Lưu ý: Đổi "chart1" thành tên thật cái biểu đồ cột của bạn trên Design
+                chart1.DataSource = dtDoanhThu;
+
+                // Gắn cột Ngay vào trục X, cột DoanhThu vào trục Y
+                chart1.Series["Doanh thu"].XValueMember = "Ngay";
+                chart1.Series["Doanh thu"].YValueMembers = "DoanhThu";
+
+                chart1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tải biểu đồ doanh thu: " + ex.Message);
+            }
+        }
+
+        // 4. ĐỔ DỮ LIỆU VÀO BIỂU ĐỒ TRÒN (TOP SẢN PHẨM BÁN CHẠY)
+        private void LoadChartTopSanPham()
+        {
+            try
+            {
+                var dtTopSP = _thongKeBus.LayTop5BanChay();
+
+                // Lưu ý: Đổi "chart2" thành tên thật cái biểu đồ tròn của bạn trên Design
+                chart2.DataSource = dtTopSP;
+
+                // Gắn Tên Sản Phẩm để phân ranh giới, gắn Số Lượng Bán để vẽ độ to của miếng bánh
+                chart2.Series["Series1"].XValueMember = "TenSanPham";
+                chart2.Series["Series1"].YValueMembers = "SoLuongBan";
+
+                // Hiện số lên từng miếng bánh
+                chart2.Series["Series1"].IsValueShownAsLabel = true;
+
+                chart2.DataBind();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tải biểu đồ top sản phẩm: " + ex.Message);
+            }
+        }
+
+        private void LoadDanhSachNhanVien()
+        {
+            try
+            {
+                // Gọi hàm từ TaiKhoanBUS của bạn
+                var dtStaff = _taiKhoanBus.LayDanhSachNhanVien();
+                dgvStaff.DataSource = dtStaff;
+
+                // --- BẢO MẬT: ẨN CÁC CỘT KHÔNG CẦN THIẾT ---
+                if (dgvStaff.Columns["matKhau"] != null)
+                    dgvStaff.Columns["matKhau"].Visible = false;
+
+                // --- TÚT LẠI LƯỚI CHO ĐẸP ---
+                dgvStaff.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dgvStaff.ReadOnly = true;
+                dgvStaff.AllowUserToAddRows = false;
+                dgvStaff.RowHeadersVisible = false;
+                dgvStaff.BackgroundColor = System.Drawing.Color.White;
+                dgvStaff.BorderStyle = BorderStyle.None;
+                dgvStaff.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+                dgvStaff.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tải danh sách nhân viên: " + ex.Message);
+            }
+        }
     }
 }
+
